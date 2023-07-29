c28b5aac3c8bbafbd81fd0db6183dcb49646b269
Thirayu Wanneewetchasin <tapetong9@gmail.com>
2023-07-29 12:35:24 +0700

    added play success sound when approved

--- axons_flutter/lib/screens/authenication/registration_stepper_screen.dart
+++ axons_flutter/lib/screens/authenication/registration_stepper_screen.dart
@@ -1,9 +1,12 @@
+import 'dart:developer';
+
 import 'package:axons_flutter/enums/registration_stepper_enum.dart';
 import 'package:axons_flutter/extensions/context_extensions.dart';
 import 'package:axons_flutter/extensions/image_extensions.dart';
 import 'package:axons_flutter/screens/authenication/widgets/registration_stepper_item.dart';
 import 'package:axons_flutter/utils/navigation/navigate_util.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import '../../widgets/appbar/custom_appbar.dart';
 import '../../widgets/button/primary_button.dart';
@@ -28,94 +31,115 @@ class _RegistrationStepperScreenState extends State<RegistrationStepperScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              width: double.infinity,
-              color: Colors.white,
-              padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const RegistrationStepperItem(status: RegistrationStepperEnum.registered),
-                  _buildDivider(true),
-                  const RegistrationStepperItem(status: RegistrationStepperEnum.waiting),
-                  _buildDivider(false),
-                  RegistrationStepperItem(status: _approved ? RegistrationStepperEnum.approved : RegistrationStepperEnum.unapproved),
-                ],
-              ),
-            ),
+            _buildStepperView(),
             Expanded(
               child: SingleChildScrollView(
                 child: Container(
                   width: double.infinity,
                   padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
                   color: Colors.grey.shade200,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset("registration_waiting_approve.png".assetPath, width: 230),
-                      const VSpacer(16),
-                      Text(
-                        tr("registration_stepper_page.title"),
-                        style: context.textTheme.titleLarge,
-                        textAlign: TextAlign.center,
-                      ),
-                      const VSpacer(8),
-                      Text(
-                        tr("registration_stepper_page.subtitle"),
-                        style: context.textTheme.bodyMedium?.copyWith(color: Colors.blueGrey.shade600),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
+                  child: _buildWaitingLandingView(),
                 ),
               ),
             ),
-            Column(
-              children: [
-                PrimaryButton(
-                  margin: const EdgeInsets.symmetric(horizontal: 16.0),
-                  onPressed: _onTapSelfApproveButton,
-                  theme: PrimaryButtonTheme.white,
-                  showBorder: true,
-                  title: "Self Approve",
-                ),
-                PrimaryButton(
-                  margin: const EdgeInsets.all(16.0),
-                  icon: const Icon(Icons.arrow_back_ios_new, size: 12),
-                  onPressed: _onTapBackButton,
-                  title: tr("registration_stepper_page.back_button"),
-                ),
-              ],
-            )
+            _buildButtons(),
           ],
         ),
       ),
     );
   }
 
-  Expanded _buildDivider(bool isHighlight) => Expanded(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 10.0),
-          child: Divider(
-            thickness: 1.0,
-            color: isHighlight ? Colors.blueGrey.shade300 : null,
-          ),
+  Widget _buildWaitingLandingView() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset("registration_waiting_approve.png".assetPath, width: 230),
+        const VSpacer(16),
+        Text(
+          tr("registration_stepper_page.title"),
+          style: context.textTheme.titleLarge,
+          textAlign: TextAlign.center,
         ),
-      );
+        const VSpacer(8),
+        Text(
+          tr("registration_stepper_page.subtitle"),
+          style: context.textTheme.bodyMedium?.copyWith(color: Colors.blueGrey.shade600),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildStepperView() {
+    return Container(
+      width: double.infinity,
+      color: Colors.white,
+      padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const RegistrationStepperItem(status: RegistrationStepperEnum.registered),
+          _buildDivider(true),
+          const RegistrationStepperItem(status: RegistrationStepperEnum.waiting),
+          _buildDivider(false),
+          RegistrationStepperItem(status: _approved ? RegistrationStepperEnum.approved : RegistrationStepperEnum.unapproved),
+        ],
+      ),
+    );
+  }
+
+  Expanded _buildDivider(bool isHighlight) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10.0),
+        child: Divider(
+          thickness: 1.0,
+          color: isHighlight ? Colors.blueGrey.shade300 : null,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButtons() {
+    return Column(
+      children: [
+        PrimaryButton(
+          margin: const EdgeInsets.symmetric(horizontal: 16.0),
+          onPressed: _onTapSelfApproveButton,
+          theme: PrimaryButtonTheme.white,
+          showBorder: true,
+          title: "Self Approve",
+        ),
+        PrimaryButton(
+          margin: const EdgeInsets.all(16.0),
+          icon: const Icon(Icons.arrow_back_ios_new, size: 12),
+          onPressed: _onTapBackButton,
+          title: tr("registration_stepper_page.back_button"),
+        ),
+      ],
+    );
+  }
 
   void _onTapSelfApproveButton() async {
-    await Future.delayed(const Duration(milliseconds: 200));
+    if (_approved) return;
 
-    setState(() {
-      _approved = true;
-    });
+    _approved = true;
 
-    Future.delayed(const Duration(milliseconds: 1500)).then(
-      (_) {
-        NavigateUtil().pop(context);
-      },
-    );
+    try {
+      final player = AudioPlayer();
+      await player.play(AssetSource('audios/success.mp3'), volume: 0.5);
+      await Future.delayed(const Duration(milliseconds: 200));
+      setState(() {});
+
+      Future.delayed(const Duration(milliseconds: 2000)).then(
+        (_) {
+          NavigateUtil().pop(context);
+        },
+      );
+    } catch (e) {
+      log('[_onTapSelfApproveButton][error]: ${e.toString()}');
+    }
   }
 
   void _onTapBackButton() => NavigateUtil().pop(context);
